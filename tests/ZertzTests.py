d354b685224569a26a8b26e6a5a35d6111d70ca9
culk <culk@users.noreply.github.com>
2018-02-25 15:09:11 -0800

    Expanded test cases
    Added tests for separable regions of the board and taking capture
    actions.

--- tests/ZertzTests.py
+++ tests/ZertzTests.py
@@ -5,6 +5,7 @@ import numpy as np
 
 from zertz.ZertzLogic import Board
 from zertz.ZertzGame import ZertzGame
+from zertz.ZertzPlayer import Player
 
 
 class TestZertzLogic(unittest.TestCase):
@@ -58,7 +59,7 @@ class TestZertzLogic(unittest.TestCase):
         self.assertEqual(board.board_state[4, 2], 0)
         self.assertTrue(board._is_removable((3, 2)))
 
-    def test_capture_moves(self):
+    def test_get_capture_moves(self):
         board = Board(19)
         board.take_action((('PUT', 'w', (4, 4)), ('REM', (4, 3))), None)
         board.take_action((('PUT', 'b', (3, 4)), ('REM', (4, 2))), None)
@@ -75,6 +76,36 @@ class TestZertzLogic(unittest.TestCase):
         for move in board._get_capture_moves():
             self.assertTrue(move in actions)
 
+    def test_separated_board_simple(self):
+        board = Board(19)
+        board.take_action((('PUT', 'b', (0, 0)), ('REM', (3, 1))), None)
+        board.take_action((('PUT', 'b', (0, 2)), ('REM', (2, 0))), None)
+        board.take_action((('PUT', 'b', (2, 4)), ('REM', (2, 1))), None)
+        board.take_action((('PUT', 'b', (4, 2)), ('REM', (3, 2))), None)
+        self.assertEqual(board.board_state[4, 2], 4)
+        player = Player(None, 1)
+        board.take_action((('PUT', 'b', (2, 2)), ('REM', (4, 3))), player)
+        self.assertEqual(board.board_state[4, 2], 0)
+        self.assertEqual(player.captured['b'], 1)
+
+    def test_separated_board_complex(self):
+        board = Board(19)
+        board.take_action((('PUT', 'b', (0, 0)), ('REM', (3, 1))), None)
+        board.take_action((('PUT', 'b', (0, 2)), ('REM', (2, 0))), None)
+        board.take_action((('PUT', 'b', (2, 4)), ('REM', (2, 1))), None)
+        board.take_action((('PUT', 'w', (4, 2)), ('REM', (3, 2))), None)
+        board.take_action((('PUT', 'b', (2, 2)), ('REM', (4, 4))), None)
+        board.take_action((('PUT', 'b', (1, 3)), ('REM', (3, 4))), None)
+        self.assertEqual(board.board_state[4, 2], 2)
+        player = Player(None, 1)
+        board.take_action((('PUT', 'b', (0, 1)), ('REM', (3, 3))), player)
+        self.assertEqual(player.captured['w'], 0)
+        self.assertEqual(board.board_state[4, 2], 2)
+        self.assertEqual(board.board_state[4, 3], 1)
+        board.take_action((('PUT', 'w', (4, 3)), ('REM', (1, 0))), player)
+        self.assertEqual(board.board_state[4, 2], 0)
+        self.assertEqual(player.captured['w'], 2)
+
 class TestZertzGame(unittest.TestCase):
     def test_init(self):
         game = ZertzGame(19)
@@ -88,6 +119,17 @@ class TestZertzGame(unittest.TestCase):
         self.assertEqual(len(game.get_valid_actions()), 648)
         game = ZertzGame(37)
         self.assertEqual(len(game.get_valid_actions()), 1944)
+        game.get_next_state((('PUT', 'w', (4, 4)), ('REM', (4, 3))))
+        game.get_next_state((('PUT', 'b', (3, 4)), ('REM', (4, 2))))
+        game.get_next_state((('PUT', 'g', (2, 3)), ('REM', (1, 3))))
+        game.get_next_state((('PUT', 'b', (1, 1)), ('REM', (3, 1))))
+        game.get_next_state((('PUT', 'b', (2, 1)), ('REM', (0, 2))))
+        self.assertEqual(len(game.get_valid_actions()), 6)
+        game = ZertzGame(1)
+        self.assertEqual(len(game.get_valid_actions()), 3)
+        put, rem = game.get_valid_actions()[0]
+        _, rem_index = rem
+        self.assertTrue(rem_index is None)
 
     def test_take_actions(self):
         game = ZertzGame(19)
@@ -99,6 +141,14 @@ class TestZertzGame(unittest.TestCase):
         self.assertTrue(np.all(supply[:3] == [5, 7, 7]))
         self.assertEqual(np.sum(board), 26)
         self.assertEqual(player, -1)
+        supply, board, player = game.get_next_state((('CAP', 'w', (4, 4)), ('b', (2, 4)), ('g', (2, 2)), ('b', (2, 0))))
+        self.assertTrue(np.all(supply[-3:] == [0, 1, 2]))
+        self.assertEqual(np.sum(board), 18)
+        self.assertTrue(board[2, 0] == 2)
+        self.assertTrue(board[4, 4] == 1)
+        self.assertTrue(board[2, 3] == 1)
+        self.assertTrue(board[1, 1] == 4)
+        self.assertEqual(player, 1)
 
         
 if __name__ == '__main__':
