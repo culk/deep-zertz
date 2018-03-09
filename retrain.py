483410fd616d1280a87d1c7f75ea3fa361b55920
culk <culk@users.noreply.github.com>
2018-03-09 14:02:59 -0800

    Style improvements

--- retrain.py
+++ retrain.py
@@ -9,36 +9,31 @@ class Coach(object):
         self.game = game
         self.model = model
         self.config = config
-        self.pmodel = self.model.__class__(self.game, self.config)
-
-        self.mcts = MCTS(self.game, self.model, self.config.c_puct, self.config.num_sims)
+        self.prev_model = self.model.__class__(self.game, self.config)
 
     def learn(self):
         for i in range(self.config.num_iters):
-            # TODO: double check if examples have been shuffled
             self_play = SelfPlay(self.game, self.model)
-            print('.')
             examples = self_play.generate_play_data()
-            print('.')
+            examples = self.shuffle_examples(examples)
 
             # Step 1. Keep a copy of the current model
             self.model.save_checkpoint(filename='temp.pth.tar')
-            self.pmodel.load_checkpoint(filename='temp.pth.tar')
+            self.prev_model.load_checkpoint(filename='temp.pth.tar')
 
             # Step 2. Training the model
-            pmcts = MCTS(self.game, self.pmodel, self.config.c_puct, self.config.num_sims)
+            prev_mcts = MCTS(self.game, self.prev_model, self.config.c_puct, self.config.num_sims)
             self.model.train(examples)
-            nmcts = MCTS(self.game, self.model, self.config.c_puct, self.config.num_sims)
-
+            new_mcts = MCTS(self.game, self.model, self.config.c_puct, self.config.num_sims)
 
             # Step 3. Evaluate the model
             print 'PITTING AGAINST PREVIOUS VERSION'
-            arena = Arena(self.game, nmcts, pmcts)
+            arena = Arena(self.game, new_mcts, prev_mcts)
             # Player 1 is the optimized player
             player1_win, player2_win, draw = arena.play_matches(self.config.arena_games)
-            print 'NEW/PREV WINS : %d / %d ; DRAWS : %d' % (player1_win, player2_win, draw)
+            print 'NEW MODEL/PREV MODEL WINS : %d / %d ; DRAWS : %d' % (player1_win, player2_win, draw)
 
-            if (player1_win * 1.0) / self.config.arena_games > self.config.arena_threshold:
+            if ((player1_win * 1.0) / self.config.arena_games) > self.config.arena_threshold:
                 print 'ACCEPTING NEW MODEL'
                 self.model.save_checkpoint(filename=self.getCheckpointFile(i))
                 self.model.save_checkpoint(filename='best.pth.tar')
@@ -46,6 +41,9 @@ class Coach(object):
                 print 'REJECTING NEW MODEL'
                 self.model.load_checkpoint(filename='temp.pth.tar')
 
+    def shuffle_examples(self, examples):
+        # TODO: implement
+        return examples
 
     def getCheckpointFile(self, iteration):
         return 'checkpoint_' + str(iteration) + '.pth.tar'
