b89db46a15162f089faaebdfd7cd8c30b41df9f7
culk <culk@users.noreply.github.com>
2018-03-12 22:40:35 -0700

    Stub code

---
+++ tests/MCTSTests.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('.')
+import unittest
+import numpy as np
+
+from mcts import Node, MCTS
+
+
+class TestMCTS(unittest):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+
